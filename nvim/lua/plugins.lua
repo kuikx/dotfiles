0b0361d559d5091ecdccf09a89c79d5557646072
saayuki <kuikuiqwo@gmail.com>
2024-04-17 18:43:40 +0800

    add some config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,46 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "tanvirtin/monokai.nvim",
+    {
+		"onsails/lspkind.nvim",
+		event = { "VimEnter" },
+	},
+    -- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "lspkind.nvim" },
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	{ "hrsh7th/cmp-nvim-lsp", dependencies = { "nvim-cmp" } },
+	{ "hrsh7th/cmp-buffer", dependencies = { "nvim-cmp" } }, -- buffer auto-completion
+	{ "hrsh7th/cmp-path", dependencies = { "nvim-cmp" } }, -- path auto-completion
+	{ "hrsh7th/cmp-cmdline", dependencies = { "nvim-cmp" } }, -- cmdline auto-completion
+	-- Code snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+	},
+    --neovim markdown preview
+    {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+    },
+
+})
+
